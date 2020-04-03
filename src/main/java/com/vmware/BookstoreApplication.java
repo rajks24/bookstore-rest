61cbbf0a311887eb4bcc5e5a3c1c46af0cccf0f1
Rajesh Kumar Singh <srajeshkumar@vmware.om>
2020-04-03 15:14:10 +0800

    updated with properties

--- src/main/java/com/vmware/BookstoreApplication.java
+++ src/main/java/com/vmware/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.vmware.models.Book;
 import com.vmware.repos.BookRepository;
@@ -17,6 +19,8 @@ public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+	
+	
 	
 	@Bean
     CommandLineRunner init (BookRepository bookRepo){
